70c990efd95f0442da6578c971b1a5e874295963
IsabelleRey <isabellrey@gmail.com>
2015-04-08 17:20:53 -0700

    gitignore

--- pok/views.py
+++ pok/views.py
@@ -3,7 +3,7 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from pok.models import Pokemon
+from pok.models import Pokemon, Team
 
 
 def all_pokemon(request):
